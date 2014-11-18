9fed25cd3c3f0d1a878904512f8c2f2637f656c3
Russley Shaw <russleyshaw@gmail.com>
2014-11-18 07:09:39 -0600

    Added more GitHub functions.
    Commiting so if my laptop explodes, I dont lose everything.

--- TeamApp/App.js
+++ TeamApp/App.js
@@ -1,57 +1,38 @@
 var Request = require('request');
 var Express = require('express');
-var ExpressSession = require('express-session');
+var CookieSession = require('cookie-session');
 var QueryString = require('querystring');
 var Jade = require('jade');
 var Fs = require('fs');
+
+var GitHub = require('./GitHub');
 
 var App = Express();
 
 // *************************************** JADE COMPILATION ***************************************
 var index_fn = Jade.compileFile('jade_templates/index.jade');
-var app_fn = Jade.compileFile('jade_templates/app.jade');
+
+var app_fn = Jade.compileFile('jade_templates/app/index.jade');
+var apporg_fn = Jade.compileFile('jade_templates/app/org.jade');
+
+var me_fn = Jade.compileFile('jade_templates/me.jade');
 
 // *************************************** GLOBALS ***************************************
 var secret_json = JSON.parse(Fs.readFileSync('Secret.json'));
 var client_id = secret_json['client_id'];
 var client_secret = secret_json['client_secret'];
 
-// *************************************** AUTH ***************************************
-function getOauthCode(req, res) {
-    var uri = 'https://github.com/login/oauth/authorize';
-    var params = {
-        "client_id": client_id,
-        "redirect_uri": 'http://localhost:3000/auth'
-    };
-    uri = uri + '?' + QueryString.stringify(params);
-    res.redirect(uri);
-}
-function getOauthAccessToken(req, res) {
-    var uri = 'https://github.com/login/oauth/access_token';
-    var params = {
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "code": req.query.code,
-        "redirect_uri": 'http://localhost:3000/app'
-    };
-    uri = uri + '?' + QueryString.stringify(params);
-
-    Request.post(uri, function(err, resp, body){
-        var query = QueryString.parse(body);
-        resp.session.access_token = query['access_token'];
-
-        res.redirect('/app');
-    });
-}
-
 // *************************************** USE METHODS ***************************************
-
-App.use(ExpressSession( {secret: 'BGT-TeamApp-SessionKey'} ));
 
 App.use(function(req, res, next){
     console.log('%s - %s - %s', req.method, req.path, req.ip);
     next();
 });
+
+App.use(CookieSession({
+    secret: 'BGT-Cookie-Session'
+}));
+
 
 // *************************************** GET METHODS ***************************************
 
@@ -59,28 +40,83 @@ App.get('/', function (req, res) {
     var locals = {
         page_title: 'Home'
     };
-
     res.send( index_fn(locals) );
 });
 
-//Handle Oauth stuff, send to /app if successful.
+//HANDLE OAUTH TO GITHUB
 App.get('/auth', function(req, res) {
-    if(req.query.code) {
-        getOauthCode(req, res);
+    var uri = null;
+    var params = null;
+    if(req.query.code === null || req.query.code === undefined) {
+        // GET GITHUB CODE
+        uri = 'https://github.com/login/oauth/authorize';
+        params = {
+            "client_id": client_id,
+            "redirect_uri": 'http://localhost:3000/auth'
+        };
+        uri = uri + '?' + QueryString.stringify(params);
+        res.redirect(uri);
     } else {
-        getOauthAccessToken(req, res);
+        //GET GITHUB ACCESS TOKEN
+        uri = 'https://github.com/login/oauth/access_token';
+        params = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "code": req.query.code,
+            "redirect_uri": 'http://localhost:3000/app'
+        };
+        uri = uri + '?' + QueryString.stringify(params);
+
+        Request.post(uri, function(err, resp, body){
+            var query = QueryString.parse(body);
+            req.session.access_token = query['access_token'];
+
+            res.redirect('/app');
+        });
     }
 
 });
 
+// *************************************** APP ***************************************
 App.get('/app', function(req, res) {
     var locals = {
-        page_title: 'App'
+        "PAGE_TITLE": "app"
     };
 
-    res.send( app_fn(locals) );
+    var WAIT_DONE = 0;
+    var WAIT_TODO = 2;
+    var WAIT_FUNC = function(){
+        WAIT_DONE++;
+        if(WAIT_DONE >= WAIT_TODO) {
+            res.send( app_fn(locals) );
+        }
+    };
+
+    BasicGitHub.GetUser(req.session.access_token, function(err, user){
+        if(err) {console.log(err);}
+        locals.GH_AUTHENTICATED_USER = user;
+        WAIT_FUNC();
+    });
+
+    BasicGitHub.GetDetailedUserOrgs(req.session.access_token, function(err, orgs){
+        if(err) {console.log(err);}
+        locals.GH_DETAILED_ORGS = orgs;
+        WAIT_FUNC();
+    })
 });
 
+// *************************************** APP/ORG/:LOGIN ***************************************
+App.get('/app/org/:login', function(req, res) {
+    var login = req.params.login;
+    var locals = {
+        "PAGE_TITLE": "Org"
+    };
+
+    BasicGitHub.
+
+    res.send( apporg_fn(locals) );
+
+});
 
 var Server = App.listen(3000, function () {
 
--- TeamApp/GitHub.js
+++ TeamApp/GitHub.js
@@ -1,7 +1,153 @@
 var Request = require('request');
+var Util = require('util');
 
 function GitHub() {
-    this.GetAuthenticatedUser = function(access_token, callback) {
+    this.base_uri = 'https://api.github.com';
 
-    }
+    // callback(error, data)
+    this._get = function(path, success_code, access_token, callback) {
+        var options = {
+            'url': this.base_uri + path,
+            "headers": {
+                "authorization": Util.format("token %s", access_token),
+                "accept": "application/vnd.github.v3+json",
+                "content-type": "application/json",
+                "user-agent": "BetterGithubTeams"
+            }
+        };
+        Request.get(options, function(error, response, body){
+            if(error) {
+                console.error(error);
+                callback(error, JSON.parse(body));
+            } else if (response.statusCode !== success_code) {
+                console.error('Status Code: '+response.statusCode+' - '+response.statusText);
+                callback('Status Code: '+response.statusCode+' - '+response.statusText, JSON.parse(body));
+            } else {
+                callback(error, JSON.parse(body));
+            }
+        });
+    };
+
+    // Gets the Authenticated User
+    // callback(err, user)
+    this.GetUser = function(access_token, callback) {
+        this._get('/user', 200, access_token, function(err, body) {
+            callback(err, body);
+        });
+    };
+
+    // Gets the user by username
+    // callback(err, user)
+    this.GetUserByUsername = function(username, access_token, callback) {
+        this._get(
+            Util.format('/users/%s', username),
+            200, access_token,
+            function(err, body) {
+                callback(err, body);
+            }
+        );
+    };
+
+    // Get the Organizations of the authenticated user.
+    // callback(err, [orgs])
+    this.ListUserOrgs = function(access_token, callback) {
+        this._get('/user/orgs', 200, access_token, function(err, body){
+            callback(err, body);
+        });
+    };
+
+    // Gets the Organization by the org login
+    // callback(err, detailed_org)
+    this.GetOrg = function(login, access_token, callback) {
+        this._get(
+            Util.format('/orgs/%s',login),
+            200, access_token,
+            function(err, body){
+                callback(err, body);
+            }
+        );
+    };
+
+    // Get teams by org login
+    // callback(err, [teams])
+    this.ListTeams = function(login, access_token, callback) {
+        this._get(
+            Util.format('/orgs/%s/teams', login),
+            200, access_token,
+            function(err, body){
+                callback(err, body);
+            }
+        );
+    };
+
+    // Get team by id
+    // callback(err, team)
+    this.GetTeam = function(id, access_token, callback) {
+        this._get(
+            Util.format('/teams/%s', id),
+            200, access_token,
+            function(err, body) {
+                callback(err, body);
+            }
+        );
+    };
+
+
+    //callback(err, [members])
+    this.GetTeamMembers = function(id, access_token, callback) {
+        var self = this;
+
+        this._get(
+            Util.format('/teams/%d/members', id),
+            200, access_token,
+            function(err, body) {
+                callback(err, body);
+            }
+        );
+    };
+
+    this.ListGists = function(access_token, callback) {
+        this._get(
+            '/gists',
+            200, access_token,
+            function(err, body) {
+                callback(err, body);
+            }
+        );
+    };
+
+    this.GetGist = function(id, access_token, callback) {
+        this._get(
+            Util.format('/gists/%s', id),
+            200, access_token,
+            function(err, body) {
+                callback(err, body);
+            }
+        );
+    };
+
+    /**************************************************************************
+     * Advanced Github
+     */
+
+    // callback(err, teams)
+    this.GetTeamWithMembers = function(id, access_token, callback) {
+
+        var _team = null;
+        var _members = [];
+
+        var SYNC_TODO = 2;
+        var SYNC_DONE = 0;
+        var SYNC_FUNC = function() {
+            SYNC_DONE += 1;
+            if(SYNC_DONE >= SYNC_TODO) {
+
+            }
+        }
+
+
+
+    };
 }
+
+module.exports = new GitHub();
