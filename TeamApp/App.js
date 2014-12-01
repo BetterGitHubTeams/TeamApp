d22ef409831daa041a3a9b4d58a55a16c15efc66
Russley Shaw <russleyshaw@gmail.com>
2014-11-30 21:17:57 -0600

    Huge Push (see commit)
    Authenticates with GitHub.
    Creates bgt.json
    Deletes bgt.json
    Added Bootstrap for styling
    Added jade files.

--- TeamApp/App.js
+++ TeamApp/App.js
@@ -1,60 +1,69 @@
 var Request = require('request');
 var Express = require('express');
 var CookieSession = require('cookie-session');
+var ServeStatic = require('serve-static');
 var QueryString = require('querystring');
 var Jade = require('jade');
 var Fs = require('fs');
+var Util = require('util');
 
 var GitHub = require('./GitHub');
 
 var App = Express();
 
 // *************************************** JADE COMPILATION ***************************************
-var index_fn = Jade.compileFile('jade_templates/index.jade');
-
-var app_fn = Jade.compileFile('jade_templates/app/index.jade');
-var apporg_fn = Jade.compileFile('jade_templates/app/org.jade');
-
-var me_fn = Jade.compileFile('jade_templates/me.jade');
+var jade_fns = {};
+jade_fns['/'] = Jade.compileFile('jade/index.jade');
+jade_fns['/dashboard'] = Jade.compileFile('jade/dashboard.jade');
+jade_fns['/org'] = Jade.compileFile('jade/org.jade');
 
 // *************************************** GLOBALS ***************************************
 var secret_json = JSON.parse(Fs.readFileSync('Secret.json'));
 var client_id = secret_json['client_id'];
 var client_secret = secret_json['client_secret'];
 
+// *************************************** OTHER FUNCTIONS ***************************************
+function MakeWebAlert(type, message) {
+    return {"type": type, "message": message};
+}
+
 // *************************************** USE METHODS ***************************************
-
-App.use(function(req, res, next){
-    console.log('%s - %s - %s', req.method, req.path, req.ip);
-    next();
-});
-
 App.use(CookieSession({
     secret: 'BGT-Cookie-Session'
 }));
 
+App.use(function(req, res, next){
+    console.log(Util.format('%s - %s - %s', req.method, req.path, req.ip));
 
-// *************************************** GET METHODS ***************************************
+    if(req.session.web_alerts == null) {
+        req.session.web_alerts = [];
+    }
+
+    next();
+});
+
+App.use('/bootstrap', Express.static('bower_components/bootstrap/dist'));
+App.use('/jquery', Express.static('bower_components/jquery/dist'));
 
 App.get('/', function (req, res) {
     var locals = {
-        page_title: 'Home'
+        "authenticated": req.session.access_token != undefined
     };
-    res.send( index_fn(locals) );
+    res.send( jade_fns['/'](locals) );
 });
 
-//HANDLE OAUTH TO GITHUB
 App.get('/auth', function(req, res) {
     var uri = null;
     var params = null;
-    if(req.query.code === null || req.query.code === undefined) {
+    if(req.query.code == null) {
         // GET GITHUB CODE
         uri = 'https://github.com/login/oauth/authorize';
         params = {
             "client_id": client_id,
-            "redirect_uri": 'http://localhost:3000/auth'
+            "redirect_uri": 'http://localhost:3000/auth',
+            "scope": 'gist, admin:org, user'
         };
-        uri = uri + '?' + QueryString.stringify(params);
+        uri = Util.format('%s?%s', uri, QueryString.stringify(params));
         res.redirect(uri);
     } else {
         //GET GITHUB ACCESS TOKEN
@@ -63,7 +72,7 @@ App.get('/auth', function(req, res) {
             "client_id": client_id,
             "client_secret": client_secret,
             "code": req.query.code,
-            "redirect_uri": 'http://localhost:3000/app'
+            "redirect_uri": 'http://localhost:3000/dashboard'
         };
         uri = uri + '?' + QueryString.stringify(params);
 
@@ -71,58 +80,89 @@ App.get('/auth', function(req, res) {
             var query = QueryString.parse(body);
             req.session.access_token = query['access_token'];
 
-            res.redirect('/app');
+            res.redirect('/dashboard');
         });
     }
-
 });
 
-// *************************************** APP ***************************************
-App.get('/app', function(req, res) {
+App.get('/dashboard', function(req, res) {
+    if(req.session.access_token == null) {
+        res.redirect('/');
+        return;
+    }
     var locals = {
-        "PAGE_TITLE": "app"
+        "authenticated": req.session.access_token != null
     };
 
-    var WAIT_DONE = 0;
     var WAIT_TODO = 2;
-    var WAIT_FUNC = function(){
+    var WAIT_DONE = 0;
+    var WAIT_FUNC = function() {
         WAIT_DONE++;
         if(WAIT_DONE >= WAIT_TODO) {
-            res.send( app_fn(locals) );
+            res.send(jade_fns['/dashboard'](locals));
         }
     };
 
-    BasicGitHub.GetUser(req.session.access_token, function(err, user){
-        if(err) {console.log(err);}
-        locals.GH_AUTHENTICATED_USER = user;
+    GitHub.GetOrgs(req.session.access_token, function(err, orgs){
+        locals.orgs = orgs;
         WAIT_FUNC();
     });
 
-    BasicGitHub.GetDetailedUserOrgs(req.session.access_token, function(err, orgs){
-        if(err) {console.log(err);}
-        locals.GH_DETAILED_ORGS = orgs;
+    GitHub.GetBGTFile(req.session.access_token, function(err, file){
+        if(err){throw err;}
+
+        console.log(file);
+        locals.bgt_file = file;
         WAIT_FUNC();
-    })
+    });
 });
 
-// *************************************** APP/ORG/:LOGIN ***************************************
-App.get('/app/org/:login', function(req, res) {
-    var login = req.params.login;
-    var locals = {
-        "PAGE_TITLE": "Org"
-    };
+App.get('/org/:login', function(req, res){
+    res.send('org/login');
+});
 
-    BasicGitHub.
 
-    res.send( apporg_fn(locals) );
 
+App.get('/logout', function(req, res){
+    delete req.session.access_token;
+    res.redirect('/');
+});
+
+App.get('/create_bgt_file', function(req, res) {
+    if(req.session.access_token == null) {
+        res.redirect('/');
+        return;
+    }
+
+    GitHub.CreateBGTFile(req.session.access_token, function(err){
+        if(err){throw err;}
+        res.redirect('/dashboard');
+    });
+});
+
+App.get('/delete_bgt_file', function(req, res) {
+    if(req.session.access_token == null) {
+        res.redirect('/');
+        return;
+    }
+
+    GitHub.DeleteBGTFile(req.session.access_token, function(err){
+        if(err){throw err;}
+
+        res.redirect('/dashboard');
+    });
+});
+
+App.get('/test', function(req, res){
+    GitHub.DeleteGist('ca513a3c0ab41901cb79', req.session.access_token, function(err){
+        if(err){throw err;}
+
+        res.send('Deleted');
+    });
 });
 
 var Server = App.listen(3000, function () {
-
     var host = Server.address().address;
     var port = Server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port)
-
+    console.log(Util.format('Example app listening at http://%s:%s', host, port));
 });
--- TeamApp/GitHub.js
+++ TeamApp/GitHub.js
@@ -1,11 +1,13 @@
 var Request = require('request');
 var Util = require('util');
+var _ = require('lodash');
 
 function GitHub() {
     this.base_uri = 'https://api.github.com';
+    this.bgt_filename = 'bgt.json';
 
-    // callback(error, data)
-    this._get = function(path, success_code, access_token, callback) {
+    // callback(err, data)
+    this._get = function (path, success_code, access_token, callback) {
         var options = {
             'url': this.base_uri + path,
             "headers": {
@@ -15,34 +17,83 @@ function GitHub() {
                 "user-agent": "BetterGithubTeams"
             }
         };
-        Request.get(options, function(error, response, body){
-            if(error) {
-                console.error(error);
-                callback(error, JSON.parse(body));
-            } else if (response.statusCode !== success_code) {
-                console.error('Status Code: '+response.statusCode+' - '+response.statusText);
-                callback('Status Code: '+response.statusCode+' - '+response.statusText, JSON.parse(body));
+        Request.get(options, function (error, response, body) {
+            if (error) {
+                callback(error, null);
+            } else if (response.statusCode != success_code) {
+                callback(response.statusText || response.statusCode, null);
             } else {
-                callback(error, JSON.parse(body));
+                callback(null, JSON.parse(body));
+            }
+        });
+    };
+
+    //callback(err, data)
+    this._post = function (path, body, success_code, access_token, callback) {
+        var options = {
+            'url': this.base_uri + path,
+            "headers": {
+                "authorization": Util.format("token %s", access_token),
+                "accept": "application/vnd.github.v3+json",
+                "content-type": "application/json",
+                "user-agent": "BetterGithubTeams"
+            },
+            "json": true,
+            "body": body
+        };
+        Request.post(options, function (error, response, body) {
+            if (error) {
+                callback(error, null);
+            } else if (response.statusCode != success_code) {
+                console.error(response.statusText || response.message);
+                callback(response.statusText || response.statusCode, null);
+            } else {
+                console.log(body);
+                callback(null, null);
+            }
+        });
+    };
+
+    //callback(err, data)
+    this._delete = function (path, success_code, access_token, callback) {
+        var options = {
+            "url": this.base_uri+path,
+            "headers": {
+                "authorization": Util.format("token %s", access_token),
+                "accept": "application/vnd.github.v3+json",
+                "content-type": "application/json",
+                "user-agent": "BetterGithubTeams"
+            }
+        };
+
+        console.log(JSON.stringify(options, null, 2));
+        Request.del(options, function (error, response, body) {
+            if (error) {
+                callback(error, null);
+            } else if (response.statusCode !== success_code) {
+                callback(response.statusText || response.statusCode, null);
+            } else {
+                console.log(body);
+                callback(null, null);
             }
         });
     };
 
     // Gets the Authenticated User
     // callback(err, user)
-    this.GetUser = function(access_token, callback) {
-        this._get('/user', 200, access_token, function(err, body) {
+    this.GetUser = function (access_token, callback) {
+        this._get('/user', 200, access_token, function (err, body) {
             callback(err, body);
         });
     };
 
     // Gets the user by username
     // callback(err, user)
-    this.GetUserByUsername = function(username, access_token, callback) {
+    this.GetUserByUsername = function (username, access_token, callback) {
         this._get(
-            Util.format('/users/%s', username),
+            '/users/'+username,
             200, access_token,
-            function(err, body) {
+            function (err, body) {
                 callback(err, body);
             }
         );
@@ -50,31 +101,59 @@ function GitHub() {
 
     // Get the Organizations of the authenticated user.
     // callback(err, [orgs])
-    this.ListUserOrgs = function(access_token, callback) {
-        this._get('/user/orgs', 200, access_token, function(err, body){
+    this.GetBriefOrgs = function (access_token, callback) {
+        this._get('/user/orgs', 200, access_token, function (err, body) {
             callback(err, body);
         });
     };
 
     // Gets the Organization by the org login
     // callback(err, detailed_org)
-    this.GetOrg = function(login, access_token, callback) {
+    this.GetOrg = function (login, access_token, callback) {
         this._get(
-            Util.format('/orgs/%s',login),
+            '/orgs/'+login,
             200, access_token,
-            function(err, body){
+            function (err, body) {
                 callback(err, body);
             }
         );
     };
 
-    // Get teams by org login
+    // Gets the Organizations of the authenticated user.
+    // callback(err, [orgs])
+    this.GetOrgs = function (access_token, callback) {
+        var self = this;
+
+        self.GetBriefOrgs(access_token, function (err, brief_orgs) {
+            var orgs = [];
+
+            var WAIT_TODO = brief_orgs.length;
+            var WAIT_DONE = 0;
+            var WAIT_FUNC = function () {
+                WAIT_DONE++;
+                if (WAIT_DONE >= WAIT_TODO) {
+                    callback(err, orgs);
+                }
+            };
+            for (var i = 0; i < brief_orgs.length; i++) {
+                self.GetOrg(brief_orgs[i].login, access_token, function (err, org) {
+                    if (err) {
+                        throw err;
+                    }
+                    orgs.push(org);
+                    WAIT_FUNC();
+                });
+            }
+        });
+    };
+
+    // Get brief teams by org login
     // callback(err, [teams])
-    this.ListTeams = function(login, access_token, callback) {
+    this.GetBriefTeams = function (login, access_token, callback) {
         this._get(
-            Util.format('/orgs/%s/teams', login),
+            '/orgs/'+login+'/teams',
             200, access_token,
-            function(err, body){
+            function (err, body) {
                 callback(err, body);
             }
         );
@@ -82,72 +161,178 @@ function GitHub() {
 
     // Get team by id
     // callback(err, team)
-    this.GetTeam = function(id, access_token, callback) {
+    this.GetTeam = function (id, access_token, callback) {
         this._get(
-            Util.format('/teams/%s', id),
+            '/teams/'+id,
             200, access_token,
-            function(err, body) {
+            function (err, body) {
                 callback(err, body);
             }
         );
     };
 
+    // Get teams of authenticated users
+    // callback(err, teams)
+    this.GetTeams = function (access_token, callback) {
+        var self = this;
+
+        self.GetBriefTeams(access_token, function (err, brief_teams) {
+            var teams = [];
+
+            var WAIT_TODO = brief_teams.length;
+            var WAIT_DONE = 0;
+            var WAIT_FUNC = function () {
+                WAIT_DONE++;
+                if (WAIT_DONE >= WAIT_TODO) {
+                    callback(err, teams);
+                }
+            };
+
+            for (var i = 0; i < brief_teams.length; i++) {
+                self.GetTeam(brief_teams[i].id, access_token, function (err, team) {
+                    teams.push(team);
+                    WAIT_FUNC();
+                });
+            }
+        });
+    };
 
     //callback(err, [members])
-    this.GetTeamMembers = function(id, access_token, callback) {
+    this.GetTeamMembers = function (id, access_token, callback) {
         var self = this;
 
         this._get(
-            Util.format('/teams/%d/members', id),
+            '/teams/'+id+'/members',
             200, access_token,
-            function(err, body) {
+            function (err, body) {
                 callback(err, body);
             }
         );
     };
 
-    this.ListGists = function(access_token, callback) {
+    // callback(err, [gists]) - gists don't have content
+    this.ListGists = function (access_token, callback) {
         this._get(
             '/gists',
             200, access_token,
-            function(err, body) {
+            function (err, body) {
                 callback(err, body);
             }
         );
     };
 
-    this.GetGist = function(id, access_token, callback) {
+    // callback(err, gist) - has content
+    this.GetGist = function (id, access_token, callback) {
         this._get(
-            Util.format('/gists/%s', id),
+            '/gists/'+id,
             200, access_token,
-            function(err, body) {
+            function (err, body) {
                 callback(err, body);
             }
         );
+    };
+
+    // callback(err);
+    this.DeleteGist = function(id, access_token, callback) {
+        var url = '/gists/'+id;
+
+        this._delete(url, 204, access_token, function(err){
+            if(err){callback(err); return;}
+
+            callback(null);
+        });
+    };
+
+    this.CreateGist = function(files, description, is_public, access_token, callback) {
+        var body = {
+            "description": description,
+            "public": is_public,
+            "files": files
+        };
+        this._post('/gists', body, 201, access_token, function(err, body){
+            if(err){callback(err, null); return;}
+
+            callback(null, body);
+        });
     };
 
     /**************************************************************************
      * Advanced Github
      */
 
-    // callback(err, teams)
-    this.GetTeamWithMembers = function(id, access_token, callback) {
-
-        var _team = null;
-        var _members = [];
-
-        var SYNC_TODO = 2;
-        var SYNC_DONE = 0;
-        var SYNC_FUNC = function() {
-            SYNC_DONE += 1;
-            if(SYNC_DONE >= SYNC_TODO) {
-
+    // callback(err, id) - file is json
+    this.GetBGTFileId = function (access_token, callback) {
+        var self = this;
+        self.ListGists(access_token, function (err, gists) {
+            if (err) {callback(err, null); return;}
+            //Check all gists
+            for(var i = 0; i < gists.length; i++) {
+                //If it has the bgt file
+                if(_.has(gists[i].files, self.bgt_filename)) {
+                    callback(null, gists[i].id);
+                    return;
+                }
             }
-        }
-
-
-
+            callback(null, null);
+        });
     };
+
+    // callback(err, file) - file is json
+    this.GetBGTFile = function(access_token, callback) {
+        var self = this;
+        self.GetBGTFileId(access_token, function(err, id){
+            console.log('GetBGTFileId callback - Err: '+err+' Id: '+id);
+
+            if(err){callback(err, null); return;}
+            if(id == null) {callback(null, null); return;}
+
+            self.GetGist(id, access_token, function(err, gist){
+                console.log('GetGist callback - Err: '+err+' Gist: '+gist);
+                if(err){callback(err, null); return;}
+
+                callback(null, JSON.parse(gist.files[self.bgt_filename].content));
+            });
+        });
+    };
+
+    // callback(err)
+    this.CreateBGTFile = function(access_token, callback) {
+        var self = this;
+        self.GetBGTFileId(access_token, function(err, id){
+            console.log('GetBGTFileId callback - Err: '+err+' Id: '+id);
+
+            if(err){callback(err); return;}
+            if(id != null){callback('Gist with file '+self.bgt_filename+' already exists.'); return;}
+
+            var files = {};
+            var desc = 'This is the Better Github Teams gist. Please don\'t modify this file.';
+            files[self.bgt_filename] = {
+                "content": '{}'
+            };
+
+            self.CreateGist(files, desc, false, access_token, function(err, data){
+                console.log('CreateGist callback - Err: '+err+' Data: '+data);
+                if(err){callback(err); return;}
+
+                callback(null);
+            })
+        });
+    };
+
+    // callback(err)
+    this.DeleteBGTFile = function(access_token, callback){
+        var self = this;
+        self.GetBGTFileId(access_token, function(err, id){
+            if(err != null){callback(err); return;}
+            if(id == null){callback('BGT file already exists.'); return;}
+
+            self.DeleteGist(id, access_token, function(err){
+                if(err != null){callback(err); return;}
+
+                callback(null);
+            })
+        });
+    }
 }
 
 module.exports = new GitHub();
